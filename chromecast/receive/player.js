313dce83b718f72e438b8a361924e5c72651ba5b
gvidas <gdambrauskas@google.com>
2015-06-17 17:32:59 -0400

    1

--- chromecast/receive/player.js
+++ chromecast/receive/player.js
@@ -46,6 +46,13 @@
 
 'use strict';
 
+var littleEndian = (function() {
+  var buffer = new ArrayBuffer(2);
+  new DataView(buffer).setInt16(0, 256, true);
+  return new Int16Array(buffer)[0] === 256;
+})();
+console.log(littleEndian); // true or false
+
 function processMetadata(type,data,timestamp) { // gvd
   var debugBytes = [];
   for (var i = 0; i < data.length; i++) {
