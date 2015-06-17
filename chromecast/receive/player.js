4f5ce67a3918b65bb921fa8465569055153152fa
gvidas <gdambrauskas@google.com>
2015-06-17 16:41:26 -0400

    1

--- chromecast/receive/player.js
+++ chromecast/receive/player.js
@@ -48,13 +48,13 @@
 
 function processMetadata(type,data,timestamp) { // gvd
   var debugBytes = [];
-  for (var i = 0; i < data.length; i++) {
-    debugBytes.push(data[i]);
-  }
-  log(debugBytes);
-  log(String.fromCharCode.apply(null, debugBytes).join(''));
-  var parser = new ima.chromecast.TxxxFrameParser(data);
-  parser.parse();   
+//  for (var i = 0; i < data.length; i++) {
+//    debugBytes.push(data[i]);
+//  }
+//  log(debugBytes);
+//  log(String.fromCharCode.apply(null, debugBytes).join(''));
+//  var parser = new ima.chromecast.TxxxFrameParser(data);
+//  parser.parse();   
 }
 
 /**
