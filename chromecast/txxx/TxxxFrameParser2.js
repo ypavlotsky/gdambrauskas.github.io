4e279470d4583d3c10d85b0af0f8609698d27937
gvidas <gdambrauskas@google.com>
2015-08-25 18:55:12 -0400

    1

--- chromecast/txxx/TxxxFrameParser2.js
+++ chromecast/txxx/TxxxFrameParser2.js
@@ -46,7 +46,6 @@ ima.chromecast = {};
 
 ima.chromecast.TxxxFrameParser = function(data/*Uint8Array*/) {
   this.data = data;
-  
   this.position = 0;
   this.limit = data.length;
 }
@@ -56,164 +55,63 @@ ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16 = 1;
 ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16BE = 2;
 ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_8 = 3;
 
-/**
- * Moves the reading offset by {@code bytes}.
- *
- * @throws IllegalArgumentException Thrown if the new position is neither in nor at the end of the
- *     array.
- */
-ima.chromecast.TxxxFrameParser.prototype.skipBytes = function(skipNumberOfBytes) {
-  this.setPosition(this.position + skipNumberOfBytes);
-}
-
-/** Reads the next byte as an unsigned value. */
-ima.chromecast.TxxxFrameParser.prototype.readUnsignedByte = function() {
-  return this.data[this.position++] & 0xFF;
-}
-
 ima.chromecast.TxxxFrameParser.prototype.parse = function() {
-
-  var id3Size = this.parseId3Header();
-  
+  var id3Size = this.parseId3Header_();
   while (id3Size > 0) {
-    var byte1 = this.readUnsignedByte();
-    var byte2 = this.readUnsignedByte();
-    var byte3 = this.readUnsignedByte();
-    var byte4 = this.readUnsignedByte();
-    var frameId = String.fromCharCode(byte1) + String.fromCharCode(byte2) + String.fromCharCode(byte3) + String.fromCharCode(byte4);
+    var byte1 = this.readUnsignedByte_();
+    var byte2 = this.readUnsignedByte_();
+    var byte3 = this.readUnsignedByte_();
+    var byte4 = this.readUnsignedByte_();
+    var frameId = String.fromCharCode(byte1) + String.fromCharCode(byte2) +
+        String.fromCharCode(byte3) + String.fromCharCode(byte4);
+    log(frameId)
     var frameSize = this.readSynchSafeInt32();
     if (frameSize <= 1) {
       break;
     }
     // Skip frame flags.
-    this.skipBytes(2);
+    this.skipBytes_(2);
     if ('TXXX' == frameId) {
-      var encoding = this.dataView.getUint8(this.position++);
-      var frameDataView = new DataView(this.readBytes(frameSize - 1));
+      var encoding = this.readUnsignedByte_();
+      log("encoding "+encoding)
+      var frameDataView = new DataView(this.readBytes(frameSize - 1).buffer);
       
       // frame has description and value.
-      var firstTerminatingNullIndex = this.indexOfTerminatingNull(frameDataView, 0, encoding);
-      var description = this.readString(frameDataView, 0, firstTerminatingNullIndex,  encoding);
-      log("description "+description)
-      var value = this.readString(frameDataView, firstTerminatingNullIndex, frameDataView.byteLength,  encoding);
-      log("value "+value)
+      var firstTerminatingNullIndex = this.indexOfTerminatingNull_(frameDataView, 0, encoding);
+      var description = this.readString_(frameDataView, 0, firstTerminatingNullIndex,  encoding);
+      log("description:"+description)
+      log("delim "+this.delimiterLength(encoding))
+      var valueStartIndex = firstTerminatingNullIndex + this.delimiterLength(encoding);
+      var value = this.readString_(frameDataView, valueStartIndex, frameDataView.byteLength,  encoding);
+      log("value:"+value)
     } else {
       // Don't care about other tags.
-      this.skipBytes(frameSize - 1); 
+      this.skipBytes_(frameSize - 1); 
     }
     id3Size -= frameSize + 10 /* header size */;
   }
 }
 
-/**
- * Sets the reading offset in the array.
- *
- * @param position Byte offset in the array from which to read.
- * @throws IllegalArgumentException Thrown if the new position is neither in nor at the end of the
- *     array.
- */
-ima.chromecast.TxxxFrameParser.prototype.setPosition = function(newPosition) {
-  if (newPosition < 0 || newPosition > this.limit) {
-    throw new Error("Invalid read");
-  }
-  this.position = newPosition;
-}
-
-/**
- * Reads the next {@code length} bytes into {@code buffer} at {@code offset}.
- *
- */
-ima.chromecast.TxxxFrameParser.prototype.readBytes = function(bytesCount) {
-  var buffer = this.data.buffer.slice(this.position, this.position + bytesCount);
-  this.position += bytesCount;
-  return buffer;
-}
-
-/**
- * Reads a Synchsafe integer.
- * <p>
- * Synchsafe integers keep the highest bit of every byte zeroed. A 32 bit synchsafe integer can
- * store 28 bits of information.
- *
- * @return The parsed value.
- */
-ima.chromecast.TxxxFrameParser.prototype.readSynchSafeInt32 = function() {
-  var byte1 = this.dataView.getUint8(this.position++);
-  var byte2 = this.dataView.getUint8(this.position++);
-  var byte3 = this.dataView.getUint8(this.position++);
-  var byte4 = this.dataView.getUint8(this.position++);
-  return (byte1 << 21) | (byte2 << 14) | (byte3 << 7) | byte4;
-}
-
-ima.chromecast.TxxxFrameParser.prototype.readString = function(view, startIndex, terminatorIndex, encoding) {
-  switch (encoding) {
-    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_ISO_8859_1:
-      return readNullTerminatedString(view, startIndex);
-
-    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16:
-    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16BE:
-      return this.readNullTerminatedStringUTF16(view, startIndex);
-
-    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_8:
-      return utf8ByteArrayToString(new Uint8Array(view.buffer.slice(startIndex, terminatorIndex)));
-       
-    default: 
-      return '';
-  }
-};
-
-ima.chromecast.TxxxFrameParser.prototype.indexOfTerminatingNull = function(view, startIndex) {
-  for (var i = startIndex; i < view.byteLength; i++) {
-    if (view.getUint8(i) == 0) {
-      return i;
-    }
-  }
-  return -1;
-};
-
-ima.chromecast.TxxxFrameParser.prototype.readNullTerminatedStringUTF16 = function(view, startIndex) {
-  var code;
-  var codes = [];
-
-  for (var i = startIndex; i < view.byteLength; i +=2) {
-    var code = dataView.getUint16(pos + i);
-    if (code == 0) break;
-    codes.push(code);
-  }
-
-  return String.fromCharCode.apply(null, codes);
-};
-
-ima.chromecast.TxxxFrameParser.prototype.indexOfTerminatingNullUTF16 = function(view, startIndex) {
-  for (var i = startIndex; i < view.byteLength; i++) {
-    if (view.getUint16(i) == 0) {
-      return i;
-    }
-  }
-  return -1;
-};
-
-ima.chromecast.TxxxFrameParser.prototype.parseId3Header = function() {
-  var byte1 = this.readUnsignedByte();
-  var byte2 = this.readUnsignedByte();
-  var byte3 = this.readUnsignedByte();
+ima.chromecast.TxxxFrameParser.prototype.parseId3Header_ = function() {
+  var byte1 = this.readUnsignedByte_();
+  var byte2 = this.readUnsignedByte_();
+  var byte3 = this.readUnsignedByte_();
   var identifier = String.fromCharCode(byte1) + String.fromCharCode(byte2) + String.fromCharCode(byte3);
   if ('ID3' != identifier) {
     throw Error("Unexpected ID3 file identifier");
   }
   
-  // Skip version, this parser handles version that truman produces.
-  this.skipBytes(2);
-
-  var flags = this.dataView.getUint8(this.position++);
-  
+  // Skip version, this parser handles version that our ad insertion system
+  // produces.
+  this.skipBytes_(2);
+  var flags = this.readUnsignedByte_();
   var id3Size = this.readSynchSafeInt32();
   
   // Check if extended header is present.
   if ((flags & 0x2) != 0) {
     var extendedHeaderSize = this.readSynchSafeInt32();
     if (extendedHeaderSize > 4) {
-      this.skipBytes(extendedHeaderSize - 4);
+      this.skipBytes_(extendedHeaderSize - 4);
     }
     id3Size -= extendedHeaderSize;
   }
@@ -224,3 +122,115 @@ ima.chromecast.TxxxFrameParser.prototype.parseId3Header = function() {
   }
   return id3Size;
 }
+
+/**
+ * Moves the reading offset by {@code bytes}.
+ *
+ * @throws IllegalArgumentException Thrown if the new position is neither in nor at the end of the
+ *     array.
+ */
+ima.chromecast.TxxxFrameParser.prototype.skipBytes_ = function(skipNumberOfBytes) {
+  this.setPosition(this.position + skipNumberOfBytes);
+}
+
+/** Reads the next byte as an unsigned value. */
+ima.chromecast.TxxxFrameParser.prototype.readUnsignedByte_ = function() {
+  return this.data[this.position++] & 0xFF;
+}
+
+/**
+ * Sets the reading offset in the array.
+ *
+ * @param position Byte offset in the array from which to read.
+ * @throws IllegalArgumentException Thrown if the new position is neither in nor at the end of the
+ *     array.
+ */
+ima.chromecast.TxxxFrameParser.prototype.setPosition = function(newPosition) {
+  if (newPosition < 0 || newPosition >= this.limit) {
+    throw new Error("Invalid read");
+  }
+  this.position = newPosition;
+}
+
+/**
+ * Reads the next {@code length} bytes into {@code buffer} at {@code offset}.
+ *
+ */
+ima.chromecast.TxxxFrameParser.prototype.readBytes = function(bytesCount) {
+  var buffer = new ArrayBuffer(bytesCount);
+  var read = new Uint8Array(buffer);
+  var j = 0;
+  for (var i = this.position; i < (this.position + bytesCount); i++) {
+    read[j++] = this.data[i];
+  }
+  return read;
+}
+
+/**
+ * Reads a Synchsafe integer. Synchsafe integers keep the highest bit of every
+ * byte zeroed. A 32 bit synchsafe integer can store 28 bits of information.
+ *
+ * @return The parsed value.
+ */
+ima.chromecast.TxxxFrameParser.prototype.readSynchSafeInt32 = function() {
+  var byte1 = this.readUnsignedByte_();
+  var byte2 = this.readUnsignedByte_();
+  var byte3 = this.readUnsignedByte_();
+  var byte4 = this.readUnsignedByte_();
+  return (byte1 << 21) | (byte2 << 14) | (byte3 << 7) | byte4;
+}
+
+ima.chromecast.TxxxFrameParser.prototype.readString_ = function(view, startIndex, terminatorIndex, encoding) {
+  switch (encoding) {
+    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_ISO_8859_1:
+      return readNullTerminatedString(view, startIndex);
+
+    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16:
+    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_16BE:
+      return this.readNullTerminatedStringUTF16_(view, startIndex);
+
+    case ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_8:
+      return utf8ByteArrayToString(new Uint8Array(view.buffer.slice(startIndex, terminatorIndex)));
+       
+    default: 
+      return '';
+  }
+};
+
+ima.chromecast.TxxxFrameParser.prototype.delimiterLength = function(encoding) {
+  return (encoding == ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_ISO_8859_1
+      || encoding == ima.chromecast.TxxxFrameParser.ID3_TEXT_ENCODING_UTF_8) ? 1 : 2;
+}
+
+ima.chromecast.TxxxFrameParser.prototype.indexOfTerminatingNull_ = function(view, startIndex) {
+  for (var i = startIndex; i < view.byteLength; i++) {
+    if (view.getUint8(i) == 0) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+ima.chromecast.TxxxFrameParser.prototype.readNullTerminatedStringUTF16_ = function(view, startIndex) {
+  var code;
+  var codes = [];
+  for (var i = startIndex; i < view.byteLength; i +=2) {
+    var code = dataView.getUint16(pos + i);
+    if (code == 0) break;
+    codes.push(code);
+  }
+  return String.fromCharCode.apply(null, codes);
+};
+
+ima.chromecast.TxxxFrameParser.prototype.indexOfTerminatingNullUTF16_ = function(view, startIndex) {
+  for (var i = startIndex; i < view.byteLength; i++) {
+    if (view.getUint16(i) == 0) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+var data = new Uint8Array([73,68,51,4,0,0,0,0,0,25,84,88,88,88,0,0,0,15,0,0,3,0,103,111,111,103,108,101,95,48,48,48,49,49,0]);
+var parser = new ima.chromecast.TxxxFrameParser(data);
+parser.parse();
