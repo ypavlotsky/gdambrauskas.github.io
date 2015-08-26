3422e8b4800a7505ba4e966043eb46d3b37a743e
gvidas <gdambrauskas@google.com>
2015-08-26 14:43:11 -0400

    1

--- chromecast/receive/player.js
+++ chromecast/receive/player.js
@@ -621,7 +621,8 @@ sampleplayer.CastPlayer.prototype.preloadVideo_ = function(mediaInformation) {
   };
   var self = this;
   host.processMetadata = function(type, data, timestamp) {
-    self.receiverStreamManager_.processMetadata;
+    console.log("gvd self.receiverStreamManager_ "+self.receiverStreamManager_)
+    self.receiverStreamManager_.processMetadata(type, data, timestamp);
   };
   gvdrequeststream(this.receiverStreamManager_);
 
@@ -863,7 +864,8 @@ sampleplayer.CastPlayer.prototype.loadVideo_ = function(info) {
       });
       var self = this;
       host.processMetadata = function(type, data, timestamp) {
-        self.receiverStreamManager_.processMetadata;
+        console.log("gvd self.receiverStreamManager_ "+self.receiverStreamManager_)
+        self.receiverStreamManager_.processMetadata(type, data, timestamp);
       };
       gvdrequeststream(this.receiverStreamManager_);
       host.onError = loadErrorCallback;
