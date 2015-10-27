54c4038e996d114d0dbba4ff002323db54a16462
gvidas <gdambrauskas@google.com>
2015-10-27 19:50:33 -0400

    1

--- chromecast/receive/player.js
+++ chromecast/receive/player.js
@@ -36,8 +36,12 @@ example.Player = function(mediaElement) {
       google.ima.cast.StreamEvent.Type.LOADED,
       function(event) {
         console.log("gvd RECEIVED google.ima.cast.StreamEvent.Type.LOADED ")
-        console.log("gvd ebentype "+ event.type)
-        console.log("gvd eventdata" + event.getData())
+        console.log("gvd ebentype "+ event.type);
+        try {
+        console.log("gvd eventdata" + event.getData());
+        }catch(e){
+          console.log("gvd errrrrrrrrrrrr");
+        }
         console.log("gvd RECEIVED google.ima.cast.StreamEvent.Type.LOADED 2")
         var streamUrl = event.getData().streamUrl;
         var subtitles = event.getData().subtitles;
